7d2f51b5addfc61336e65d41f7d7d1eddcda81ef
Tarek Qassoumeh <144264754+Tarek-Qa@users.noreply.github.com>
2024-06-02 10:38:30 +0200

    Add project files.

---
+++ Project1/src/Web/Endpoints/Users.cs
@@ -0,0 +1,11 @@
+﻿using Project1.Infrastructure.Identity;
+
+namespace Project1.Web.Endpoints;
+public class Users : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .MapIdentityApi<ApplicationUser>();
+    }
+}
